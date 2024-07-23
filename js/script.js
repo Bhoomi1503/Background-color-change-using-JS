c4286f319ee3bd7917d7cbdf9a7c5d266bd4108e
Bhoomi <122143468+Bhoomi1503@users.noreply.github.com>
2024-07-23 16:51:36 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function bg(){
+    document.getElementById("body").style.backgroundColor="lightgreen";
+    document.getElementById("span").textContent="LightGreen"
+    document.getElementById("span").style.color="green";
+}
+function col(){
+    document.getElementById("body").style.backgroundColor="purple";
+    document.getElementById("span").textContent="purple";
+    document.getElementById("span").style.color="plum";
+}
+function color(){
+    document.getElementById("body").style.backgroundColor="orange";
+    document.getElementById("span").textContent="Orange";
+    document.getElementById("span").style.color="white";
+}
+function col1(){
+    document.getElementById("body").style.backgroundColor="blue";
+    document.getElementById("span").textContent="Blue";
+    document.getElementById("span").style.color="lightblue";
+}
+
